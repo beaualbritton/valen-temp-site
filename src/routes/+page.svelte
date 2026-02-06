44d37f39312ed0e4cc9afead780be7bee1353bd4
beaualbritton <albrittoncb@gmail.com>
2026-02-05 23:57:46 -0500

    fix: font icons [i cant do this webdev shit anymore](https://youtu.be/86P8SnEnF0g)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
-<main class = "flex flex-col w-full h-screen items-center justify-center">
+<main class = "flex flex-col w-full h-screen items-center justify-center font-icons">
 
   <div class="flex flex-col items-center justify-center">
     <h1 class= "text-9xl text-green font-bold">valen<span class="ml-4 font-icons text-s text-red"></h1>
     <p class="text-lg text-yellow mb-5">version control for everyone.</p>
     <div class="flex items-start gap-4 p-4 rounded-md border border-[#dbbc7f] bg-[#dbbc7f]/15 w-lg">
-      <span class="h-fit text-red text-xl font-bold 0">󰻷</span>
+      <span class="font-icons h-fit text-red text-xl font-bold 0">󰻷</span>
       <div class="flex-col">
         <p class="text-yellow">valen is coming soon, a pre-alpha should be up in the spring. <b>i'm looking for funding, so consider donating to keep things community sourced</b> </p>
         <a href="https://github.com/beaualbritton/valen" class="underline text-blue">check the github</a>
